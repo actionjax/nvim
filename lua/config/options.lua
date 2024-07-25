27c8d557e59df6756fa5ff48b60a524ea9c9b4ec
Jackson Kwok <jackson.kwok@gmail.com>
2024-07-25 10:33:01 -0400

    devnet changes

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,20 +1,36 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
+
 vim.g.clipboard = {
-  name = 'WslClipboard',
+  name = "myClipboard",
   copy = {
-    ['+'] = 'clip.exe',
-    ['*'] = 'clip.exe',
+    ["+"] = "tmux load-buffer -",
+    ["*"] = "tmux load-buffer -",
   },
   paste = {
-    ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ["+"] = "tmux save-buffer -",
+    ["*"] = "tmux save-buffer -",
   },
-  cache_enabled = 0,
+  cache_enabled = 1,
 }
-local space = '.'
-vim.opt.listchars:append({ tab = '| ', trail = space, nbsp = space })
-vim.opt.inccommand = 'split'
+
+-- vim.g.clipboard = {
+--   name = 'WslClipboard',
+--   copy = {
+--     ['+'] = 'clip.exe',
+--     ['*'] = 'clip.exe',
+--   },
+--   paste = {
+--     ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--     ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--   },
+--   cache_enabled = 0,
+-- }
+local space = "."
+vim.opt.listchars:append({ tab = "| ", trail = space, nbsp = space })
+vim.opt.inccommand = "split"
 vim.opt.cursorline = true
+vim.g.lazyvim_python_lsp = "pyright"
+vim.g.lazyvim_python_ruff = "ruff_lsp"

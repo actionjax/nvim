e6dcdb647dac97fced01a633e9c04c13d767b23f
Jackson Kwok <jackson.kwok@gmail.com>
2025-01-16 09:53:37 -0500

    chore: updated plugins

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,36 +1,22 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.clipboard = "unnamedplus"
-
-vim.g.clipboard = {
-  name = "myClipboard",
-  copy = {
-    ["+"] = "tmux load-buffer -",
-    ["*"] = "tmux load-buffer -",
-  },
-  paste = {
-    ["+"] = "tmux save-buffer -",
-    ["*"] = "tmux save-buffer -",
-  },
-  cache_enabled = 1,
-}
+-- Set up Ctrl-Copy in Visual mode
 
 -- vim.g.clipboard = {
---   name = 'WslClipboard',
+--   name = "WslClipboard",
 --   copy = {
---     ['+'] = 'clip.exe',
---     ['*'] = 'clip.exe',
+--     ["+"] = "clip.exe",
+--     ["*"] = "clip.exe",
 --   },
 --   paste = {
---     ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
---     ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--     ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--     ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
 --   },
---   cache_enabled = 0,
+--   cache_enabled = true,
 -- }
-local space = "."
-vim.opt.listchars:append({ tab = "| ", trail = space, nbsp = space })
-vim.opt.inccommand = "split"
-vim.opt.cursorline = true
-vim.g.lazyvim_python_lsp = "pyright"
-vim.g.lazyvim_python_ruff = "ruff_lsp"
+
+vim.g.minipairs_disable = true -- disable autopairs by default
+local space = "·"
+vim.opt.listchars:append({ tab = "¦ ", trail = space, nbsp = space })
+vim.g.lazyvim_blink_main = false
